18db23bab522bb23c7329bc5851171b7fa828755
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-12 13:08:28 -0500

    Revert previous layout and rebuild

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ export default function App() {
   return (
     <div className="app">
       <section className="hero">
-        <div className="hero__logo">
-          <img
-            alt="Matthew Sullivan's Logo"
-            className="hero__logo--small"
-            src="/assets/logo.png"
-          />
-        </div>
-        <div className="hero__tag">
-          <h1>Multimedia Specialist</h1>
-        </div>
+        <section className="hero__image"></section>
+        <section className="hero__content">
+          <p>
+            <strong>Matthew Sullivan</strong>
+          </p>
+          <h1>
+            <span>Create.</span>
+            <span>Enjoy.</span>
+            <span>Repeat.</span>
+          </h1>
+          <p>
+            As a <i>Design Principal</i> at <strong>IBM</strong>, Peter fuses
+            his love for design, curiosity for technology, and unwavering
+            commitment to understanding user behavior to lead future experiences
+            for <strong>IBM Security</strong>.
+          </p>
+        </section>
       </section>
       <section className="about"></section>
+      <section className="works"></section>
+      <section className="contact"></section>
     </div>
   );
 }
