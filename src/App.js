0e6b6ac4918a29fb8f6d6fde188d99b3e02c67ef
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-12 10:38:27 -0500

    Add hero and about section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="app">
-      <div className="landing"></div>
+      <section className="hero">
+        <div className="hero__logo">
+          <img
+            alt="Matthew Sullivan's Logo"
+            className="hero__logo--small"
+            src="/assets/logo.png"
+          />
+        </div>
+        <div className="hero__tag">
+          <h1>Multimedia Specialist</h1>
+        </div>
+      </section>
+      <section className="about"></section>
     </div>
   );
 }
