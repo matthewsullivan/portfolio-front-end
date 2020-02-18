20535ff44ac0c63a5cc23c66380bd3020f6dc565
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-18 12:05:47 -0500

    Add basic navigation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,30 @@ export default function App() {
           className="header__img"
           src="/assets/logo--square.png"
         />
+        <nav className="navigation">
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a className="navigation__link" href="#">
+                About
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#">
+                Folio
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
       <section className="hero"></section>
     </div>
