be218219a141d432932b1e0c475d677fe45cf9ca
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-18 11:18:07 -0500

    Cleanup and implement basic header structure

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 
+// import IconButton from '@material-ui/core/IconButton';
+
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="app">
-      <section className="hero">
-        <section className="hero__image"></section>
-        <section className="hero__content">
-          <p>
-            <strong>Matthew Sullivan</strong>
-          </p>
-          <h1>
-            <span>Create.</span>
-            <span>Enjoy.</span>
-            <span>Repeat.</span>
-          </h1>
-          <p>
-            As a <i>Design Principal</i> at <strong>IBM</strong>, Peter fuses
-            his love for design, curiosity for technology, and unwavering
-            commitment to understanding user behavior to lead future experiences
-            for <strong>IBM Security</strong>.
-          </p>
-        </section>
-      </section>
-      <section className="about"></section>
-      <section className="works"></section>
-      <section className="contact"></section>
+      <header className="header">
+        <img
+          alt="Header Logo Image"
+          className="header__img"
+          src="/assets/logo--square.png"
+        />
+      </header>
+      <section className="hero"></section>
     </div>
   );
 }
