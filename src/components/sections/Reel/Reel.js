70cc21450b110e43f34a68f5ff39bca4fb7cda96
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-19 18:15:30 -0500

    Add Demo Reel section

---
+++ src/components/sections/Reel/Reel.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ReactPlayer from 'react-player';
+
+import Title from '../../elements/Title/Title';
+
+import './Reel.css';
+
+export default function Reel() {
+  const options = {
+    url: 'https://youtu.be/nQa72e_cC5I',
+  };
+  return (
+    <section className="reel">
+      <header className="reel__header">
+        <Title body="Have a look at my demo reel." light title="Demo Reel" />
+      </header>
+      <section className="reel__content">
+        <ReactPlayer className="reel__video" {...options} />
+      </section>
+    </section>
+  );
+}
