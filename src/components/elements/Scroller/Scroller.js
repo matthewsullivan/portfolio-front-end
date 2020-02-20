0de8ef696f3368099d1fec912ae0c95305d51fa4
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-20 14:31:26 -0500

    Cleanup scroller

--- src/components/elements/Scroller/Scroller.js
+++ src/components/elements/Scroller/Scroller.js
@@ -4,81 +4,97 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-const HorizontalTranslateContainer = styled.div.attrs(({translateX}) => ({
-  style: {transform: `translateX(${translateX}px)`},
-}))`
-  height: 100%;
-  position: absolute;
-  will-change: transform;
-`;
-
-const StickyInnerContainer = styled.div`
-  height: 100vh;
-  overflow-x: hidden;
-  position: sticky;
-  top: 0;
-  width: 100%;
-`;
-
-const applyScrollListener = (ref, setTranslateX) => {
+/**
+ * Add Scroll Listener
+ * @param {object} ref
+ * @param {function} setTranslateX
+ */
+const addScrollListener = (ref, setTranslateX) => {
   window.addEventListener('scroll', () => {
     const offsetTop = -ref.current.offsetTop;
+
     setTranslateX(offsetTop);
   });
 };
 
-const TallOuterContainer = styled.div.attrs(({dynamicHeight}) => ({
-  style: {height: `${dynamicHeight}px`},
-}))`
-  position: relative;
-  width: 100%;
-`;
-
-const calcDynamicHeight = (objectWidth) => {
+/**
+ * Calculate Height
+ * @param {number} objectWidth
+ * @return {number}
+ */
+const calculateHeight = (objectWidth) => {
   const vh = window.innerHeight;
   const vw = window.innerWidth;
 
   return objectWidth - vw + vh + 150;
 };
 
-const handleDynamicHeight = (ref, setDynamicHeight) => {
+/**
+ * Handle Height
+ * @param {object} ref
+ * @param {function} setDynamicHeight
+ */
+const handleHeight = (ref, setDynamicHeight) => {
   const objectWidth = ref.current.scrollWidth;
 
-  const dynamicHeight = calcDynamicHeight(objectWidth);
+  const dynamicHeight = calculateHeight(objectWidth);
 
   setDynamicHeight(dynamicHeight);
 };
 
 Scroller.propTypes = {
-  children: PropTypes.string,
+  children: PropTypes.object,
 };
 
-export default function Scroller({children}) {
+export default function Scroller(props) {
+  const {children} = props;
+
   const [dynamicHeight, setDynamicHeight] = useState(null);
   const [translateX, setTranslateX] = useState(0);
 
   const containerRef = useRef(null);
   const objectRef = useRef(null);
 
-  const resizeHandler = () => {
-    handleDynamicHeight(objectRef, setDynamicHeight);
+  const InnerContainer = styled.div`
+    height: 100vh;
+    overflow-x: hidden;
+    position: sticky;
+    top: 0;
+    width: 100%;
+  `;
+
+  const TranslateContainer = styled.div.attrs(({translateX}) => ({
+    style: {transform: `translateX(${translateX}px)`},
+  }))`
+    height: 100%;
+    position: absolute;
+    will-change: transform;
+  `;
+
+  /**
+   * Handle Resize
+   */
+  const handleResize = () => {
+    handleHeight(objectRef, setDynamicHeight);
   };
 
   useEffect(() => {
-    handleDynamicHeight(objectRef, setDynamicHeight);
+    handleHeight(objectRef, setDynamicHeight);
 
-    window.addEventListener('resize', resizeHandler);
+    window.addEventListener('resize', handleResize);
 
-    applyScrollListener(containerRef, setTranslateX);
+    addScrollListener(containerRef, setTranslateX);
   }, []);
 
   return (
-    <TallOuterContainer dynamicHeight={dynamicHeight}>
-      <StickyInnerContainer ref={containerRef}>
-        <HorizontalTranslateContainer translateX={translateX} ref={objectRef}>
-          {children}
-        </HorizontalTranslateContainer>
-      </StickyInnerContainer>
-    </TallOuterContainer>
+    <div className="scroller">
+      <div className="scroller__outer" style={{height: `${dynamicHeight}px`}}>
+        <InnerContainer ref={containerRef}>
+          <TranslateContainer translateX={translateX} ref={objectRef}>
+            {children}
+          </TranslateContainer>
+        </InnerContainer>
+      </div>
+    </div>
   );
 }
