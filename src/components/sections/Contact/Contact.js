8a986cd39f8c488eadfffe4f06114c68f2e6d73f
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-25 12:23:51 -0500

    Update API calls

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -15,7 +15,7 @@ import './Contact.css';
 let api = 'https://www.matthewsullivan.media';
 
 if (process.env.NODE_ENV === 'development') {
-  api = 'http://127.0.0.1:5000';
+  api = 'http://127.0.0.1:8080';
 } else if (process.env.REACT_APP_ENV === 'staging') {
   api = 'https://www.staging.matthewsullivan.media';
 }
