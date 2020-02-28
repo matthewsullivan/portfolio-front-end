c8b15f939a4c095f0af9ff7ac7cbc016fb28088c
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-28 15:38:47 -0500

    Enhance compatibility

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Bowser from 'bowser';
+
 import App from './App';
+import Disabled from './components/elements/Disabled/Disabled';
 
 import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const blacklist = ['Internet Explorer', 'Microsoft Edge'];
+const browser = Bowser.getParser(window.navigator.userAgent);
+const name = browser.getBrowserName();
+
+let component = <App />;
+
+if (blacklist.includes(name)) {
+  const version = browser.getBrowserVersion();
+
+  if (version < 16) {
+    component = <Disabled />;
+  }
+}
+
+ReactDOM.render(component, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
