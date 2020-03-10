5d00c29fcb8eb82feaadb37cf5696dc4bff1f871
Matthew Sullivan <contact@matthewsullivan.media>
2020-03-10 15:34:11 -0400

    Add base reel height and lazy load card content

--- src/components/elements/Card/Card.js
+++ src/components/elements/Card/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import IosArrowRoundForward from 'react-ionicons/lib/IosArrowRoundForward';
+import LazyLoad from 'react-lazyload';
 import styled from 'styled-components';
 
 import './Card.css';
@@ -23,21 +24,23 @@ const Card = (props) => {
 
   return (
     <div className="card" data-study={1} key={study.name}>
-      <CardHeader className="card__header" study={study} />
+      <LazyLoad offset={184} once>
+        <CardHeader className="card__header" study={study} />
 
-      <div className="card__content">
-        <CardAvatar className="card__avatar" study={study} />
+        <div className="card__content">
+          <CardAvatar className="card__avatar" study={study} />
 
-        <h3 className="card__pre-title">{study.type}</h3>
-        <h2 className="card__title">{study.name}</h2>
+          <h3 className="card__pre-title">{study.type}</h3>
+          <h2 className="card__title">{study.name}</h2>
 
-        <div className="card__line" />
+          <div className="card__line" />
 
-        <p className="card__synopsis">{study.synopsis}</p>
-        <p className="card__link">
-          View Case Study <IosArrowRoundForward fontSize="24" />
-        </p>
-      </div>
+          <p className="card__synopsis">{study.synopsis}</p>
+          <p className="card__link">
+            View Case Study <IosArrowRoundForward fontSize="24" />
+          </p>
+        </div>
+      </LazyLoad>
     </div>
   );
 };
