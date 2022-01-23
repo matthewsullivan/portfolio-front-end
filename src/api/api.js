c3c7377adce901bdf23552958f225aab0dc5546e
Matthew Sullivan <contact@matthewsullivan.media>
2022-01-22 22:28:28 -0500

    Update API path

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 let api = 'http://127.0.0.1:8080';
 
-if (process.env.REACT_APP_ENV === 'production') {
-  api = 'https://www.matthewsullivan.media';
-} else if (process.env.REACT_APP_ENV === 'staging') {
-  api = 'https://www.staging.matthewsullivan.media';
+if (
+  process.env.REACT_APP_ENV === 'production' ||
+  process.env.REACT_APP_ENV === 'staging'
+) {
+  api = '/';
 }
 
 export default api;
