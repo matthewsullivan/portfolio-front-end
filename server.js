e99ffb9bcfa2e172edc10522da715309c5d55173
Matthew Sullivan <contact@matthewsullivan.media>
2022-01-22 22:13:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ if (
   app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve('build/index.html'));
+    res.sendFile(path.resolve(`${__dirname}build/index.html`));
   });
 }
 
