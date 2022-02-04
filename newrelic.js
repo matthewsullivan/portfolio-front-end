2504d7c2875b3923b3cd2d4826b738cb20d31316
Matthew Sullivan <contact@matthewsullivan.media>
2022-02-04 14:28:43 -0500

    Add license key to environment variable

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'c439490006133e75801b888ac9f4d48b4728NRAL',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
